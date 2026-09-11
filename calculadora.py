081026293d350011288aabfdeedf50f36f51c38e
Pedro Henrique <pedrohenriquesaopaulo0@github.com>
2026-09-11 19:12:09 -0300

    adicionei a subtração

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,9 @@ def soma(num1, num2):
     return soma
 
 print( "resultado da soma: ", soma(10, 5))
+
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+print("resultado da subtracao: ", subtracao(10, 5))
