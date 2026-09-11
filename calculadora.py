ed04c71bcff6acbcbbe851f03be1d2dacae12287
Pedro Henrique <pedrohenriquesaopaulo0@github.com>
2026-09-11 18:59:44 -0300

    CALCULADORA SOMA

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("### CALCULADORA PYTHON ###")
+
+def soma(num1, num2):
+    soma = num1 + num2
+    return soma
+
+print( "resultado da soma: ", soma(10, 5))
---
+++ par_impar.py
@@ -0,0 +1,5 @@
+num= int(input('digite um número inteiro: '))
+if num % 2 == 0:
+    print(f'o número {num} é par')
+else:
+    print(f'o número {num} é ímpar')
